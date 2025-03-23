45d8d73194227dcb2ec7446f4fed04c9fce8b8c0
fiax <fildajenej@gmail.com>
2025-03-23 22:49:17 +0100

    updated version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "eu.kanade.tachiyomi.faxyomi"
 
         versionCode = 72
-        versionName = "1.11.0"
+        versionName = "1.11.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
